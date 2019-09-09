b2552e90e3302132fe63c963e1aeed3c0beddb5f
sal-pal-05 <C00137488@louisiana.edu>
2019-09-09 10:14:51 -0500

    if then 1

---
+++ If:then statements script.R
@@ -0,0 +1,17 @@
+f=function () {
+  x=1
+  y=2
+  z=3
+  x+y+z+i
+}
+
+switchero.if.then=function (x) {
+  if (x=="a")
+  "camel"
+  else if(x=="b")
+    "bear"
+  else if (x=="c")
+    "alligator"
+  else
+    "moose"
+}
